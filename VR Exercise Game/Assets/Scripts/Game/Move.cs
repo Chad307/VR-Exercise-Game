92f09c2ea696518d34e44efbaa63d51296f784cc
Chad Johnson <cjohnson92130@gmail.com>
2018-04-30 13:44:07 -0700

    Add explosion effect and physics to balls

--- VR Exercise Game/Assets/Scripts/Game/Move.cs
+++ VR Exercise Game/Assets/Scripts/Game/Move.cs
@@ -6,15 +6,30 @@ public class Move : MonoBehaviour {
 
     public Transform target;
     public float speed;
+    public float detonationTime;
+    public GameObject detonationPrefab;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Awake () {
+        GetComponent<Rigidbody>().AddForce((target.position - transform.position) * speed, 
+            ForceMode.Impulse);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        float step = speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        //float step = speed * Time.deltaTime;
+        //transform.position = Vector3.MoveTowards(transform.position, target.position, step);
 	}
+
+    public void StartDetonation()
+    {
+        Instantiate(detonationPrefab, transform);
+        StartCoroutine(Detonate());
+    }
+
+    private IEnumerator Detonate()
+    {
+        yield return new WaitForSeconds(detonationTime);
+        Destroy(gameObject);
+    }
 }
---
+++ VR Exercise Game/Assets/Scripts/Player/PunchTracker.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Valve.VR.InteractionSystem;
+using UnityEngine;
+
+namespace Player
+{
+    public class PunchTracker : MonoBehaviour
+    {
+        //SteamVR_TrackedObject trackedObject;
+
+        //SteamVR_TrackedController trackedController;
+
+        public Hand hand;
+
+        public float forceMultipler;
+
+        //SteamVR_Controller.Device device;
+
+        // Use this for initialization
+        void Awake()
+        {
+            //trackedObject = GetComponent<SteamVR_TrackedObject>();
+            //trackedController = GetComponent<SteamVR_TrackedController>();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("projectile"))
+            {
+                other.GetComponent<Move>().StartDetonation();
+                Rigidbody projectileRigidbody = other.GetComponent<Rigidbody>();
+                projectileRigidbody.velocity = hand.GetTrackedObjectVelocity() * forceMultipler;
+                projectileRigidbody.angularVelocity = hand.GetTrackedObjectAngularVelocity() * forceMultipler;
+                projectileRigidbody.maxAngularVelocity = projectileRigidbody
+                    .angularVelocity.magnitude;
+                Debug.Log("Hit");
+            }
+        }
+    }
+}
